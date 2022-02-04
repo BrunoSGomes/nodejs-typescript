a8e8aadbb0bde681e4e1d425b3b08087d4fadb05
BrunoSGomes <brunogomes.dev@example.com>
2022-02-04 00:42:12 -0300

    chore(app): prettier config and first code fix

--- src/server.ts
+++ src/server.ts
@@ -6,24 +6,24 @@ import './util/module-alias';
 
 export class SetupServer extends Server {
     constructor(private port = 3010) {
-        super()
+        super();
     }
 
     public init(): void {
-        this.setupExpress()
-        this.setupControllers()
+        this.setupExpress();
+        this.setupControllers();
     }
 
     private setupExpress(): void {
-        this.app.use(bodyParser.json())
+        this.app.use(bodyParser.json());
     }
 
     private setupControllers(): void {
-        const forecastController = new ForecastController()
-        this.addControllers([forecastController])
+        const forecastController = new ForecastController();
+        this.addControllers([forecastController]);
     }
 
     public getApp(): Application {
-        return this.app
+        return this.app;
     }
 }
