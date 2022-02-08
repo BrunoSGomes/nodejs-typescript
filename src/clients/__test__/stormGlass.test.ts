9851ce36d96e5220a4b22eae25e849f7123ae480
BrunoSGomes <brunogomes.dev@example.com>
2022-02-07 22:12:49 -0300

    chore(StormGlass unit test): best option for mocking import/types

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -6,14 +6,15 @@ import stormGlassNormalized3HoursFixture from '@test/fixtures/stormglass_normali
 jest.mock('axios');
 
 describe('StormGlass client', () => {
+    const mockedAxios = axios as jest.Mocked<typeof axios> // Best option for mocking
     it('should return the normalized forecast from the StormGlass service', async () => {
         const lat = -33.345353;
         const lng = 151.323234;
 
-        axios.get = jest.fn().mockResolvedValue(stormGlassWeather3HoursFixture);
+        mockedAxios.get.mockResolvedValue({ data: stormGlassWeather3HoursFixture });
 
-        const stormGlass = new StormGlass(axios);
-        const response = await stormGlass.fetchPoints(lat, lng);
-        expect(response).toEqual(stormGlassNormalized3HoursFixture);
+        const stormGlass = new StormGlass(mockedAxios);
+        const response = await stormGlass.fetchPoints(lat, lng)
+        expect(response).toEqual(stormGlassNormalized3HoursFixture)
     });
 });
